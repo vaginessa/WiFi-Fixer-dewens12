e798add76ad5cb5bcc7711db405092981cdb2e11
David Van de Ven <zanshin.g1@gmail.com>
2010-06-16 10:29:27 -0700

    removed ReadBytes, will use another implimentation

--- wififixer/src/org/wahtod/wififixer/WifiFixerService.java
+++ wififixer/src/org/wahtod/wififixer/WifiFixerService.java
@@ -16,7 +16,6 @@
 
 package org.wahtod.wififixer;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -997,24 +996,6 @@ public class WifiFixerService extends Service {
 			
 		return START_STICKY;
 	}
-	
-	public byte[] readBytes(InputStream inputStream) throws IOException {
-		  // this dynamically extends to take the bytes you read
-		  ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-
-		  // this is storage overwritten on each iteration with bytes
-		  int bufferSize = 1024;
-		  byte[] buffer = new byte[bufferSize];
-
-		  // we need to know how may bytes were read to write them to the byteBuffer
-		  int len = 0;
-		  while ((len = inputStream.read(buffer)) != -1) {
-		    byteBuffer.write(buffer, 0, len);
-		  }
-
-		  // and then we can return your byte array.
-		  return byteBuffer.toByteArray();
-		}
 	
 	void setup() {
 		//Yeah, so the constant WIFI_MODE_FULL wasn't obvious
