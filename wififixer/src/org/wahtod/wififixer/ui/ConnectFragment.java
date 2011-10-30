fca96e03e0f4fdeac5558a240444daea123f0c80
David Van de Ven <zanshin.g1@gmail.com>
2011-10-29 20:30:21 -0700

    ConnectFragment almost working.

--- wififixer/src/org/wahtod/wififixer/ui/ConnectFragment.java
+++ wififixer/src/org/wahtod/wififixer/ui/ConnectFragment.java
@@ -26,7 +26,6 @@ import android.content.Intent;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,10 +34,13 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConnectFragment extends FragmentSwitchboard implements
 	OnClickListener {
 
+    private static final String WPA = "WPA";
+    private static final String WEP = "WEP";
     private WFScanResult network;
 
     @Override
@@ -46,6 +48,15 @@ public class ConnectFragment extends FragmentSwitchboard implements
 	    Bundle savedInstanceState) {
 	View v = inflater.inflate(R.layout.connect_fragment, null);
 	Button b = (Button) v.findViewById(R.id.connect);
+	View e = v.findViewById(R.id.password);
+	TextView summary = (TextView) v.findViewById(R.id.password_summary);
+	if (network.capabilities.length() == 0
+		|| KnownNetworksFragment.getNetworks(getActivity()).contains(
+			network.SSID)) {
+	    e.setVisibility(View.INVISIBLE);
+	    b.setText(getString(R.string.connect));
+	    summary.setText(R.string.button_connect);
+	}
 	b.setOnClickListener(this);
 	return v;
     }
@@ -73,14 +84,8 @@ public class ConnectFragment extends FragmentSwitchboard implements
 	return QUOTE + s + QUOTE;
     }
 
-    public void addNetwork(final String password) {
-	Log.i(this.getClass().getName(), password);
-	WifiConfiguration wf = new WifiConfiguration();
-	wf.SSID = addquotes(network.SSID);
-	wf.preSharedKey = addquotes(password);
-	String[] s = new String[4];
-	s[0] = addquotes(password);
-	wf.wepKeys = s;
+    private void addNetwork(final String password) {
+	WifiConfiguration wf = getKeyAppropriateConfig(password);
 	WifiManager wm = WFConnection.getWifiManager(getActivity()
 		.getApplicationContext());
 	int network = wm.addNetwork(wf);
@@ -90,29 +95,76 @@ public class ConnectFragment extends FragmentSwitchboard implements
 	}
     }
 
+    private WifiConfiguration getKeyAppropriateConfig(final String password) {
+	WifiConfiguration wf = new WifiConfiguration();
+	wf.SSID = addquotes(network.SSID);
+	if (network.capabilities.length() == 0) {
+	    wf.BSSID = addquotes(network.BSSID);
+	    wf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+	    return wf;
+	}
+	wf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+	wf.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+	wf.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+	wf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+	wf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+	wf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+	wf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+	if (network.capabilities.contains(WEP)) {
+	    wf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+	    wf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+	    wf.wepKeys[0] = addquotes(password);
+	} else if (network.capabilities.contains(WPA)) {
+	    wf.preSharedKey = addquotes(password);
+	}
+	return wf;
+    }
+
+    private void connectNetwork() {
+	Intent intent = new Intent(WFConnection.CONNECTINTENT);
+	intent.putExtra(WFConnection.NETWORKNAME, network.SSID);
+	getActivity().sendBroadcast(intent);
+    }
+
     public static ConnectFragment newInstance(Bundle bundle) {
 	ConnectFragment f = new ConnectFragment();
 	f.setArguments(bundle);
 	return f;
     }
 
+    private void notifyConnecting() {
+	Toast.makeText(
+		getActivity(),
+		getActivity().getString(R.string.connecting_to_network)
+			+ network.SSID, Toast.LENGTH_SHORT).show();
+    }
+
     public void onClick(View v) {
 	View e = ((View) v.getParent()).findViewById(R.id.password);
-	String password;
+	String password = null;
 	try {
 	    password = String.valueOf(((EditText) e).getText());
 	} catch (NullPointerException e1) {
-	    password = "";
 	}
-	addNetwork(password);
+	if (password == null || password.length() == 0) {
+	    if (network.capabilities.length() == 0) {
+		addNetwork(null);
+		notifyConnecting();
+		connectNetwork();
+	    } else if (KnownNetworksFragment.getNetworks(getActivity())
+		    .contains(network.SSID))
+		notifyConnecting();
+	    connectNetwork();
+	} else
+	    addNetwork(password);
+
 	InputMethodManager imm = (InputMethodManager) getActivity()
 		.getSystemService(Context.INPUT_METHOD_SERVICE);
 	imm.hideSoftInputFromWindow(e.getWindowToken(), 0);
 	if (getActivity().getClass().equals(GenericFragmentActivity.class))
 	    getActivity().finish();
 	else {
-	    Intent i = new Intent(getActivity(),
-		    WifiFixerActivity.class);
+	    Intent i = new Intent(getActivity(), WifiFixerActivity.class);
 	    i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 	    i.putExtra(WifiFixerActivity.REMOVE_CONNECT_FRAGMENTS, true);
 	    getActivity().startActivity(i);
