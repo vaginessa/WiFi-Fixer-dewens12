4ba8963f60ebaa7739ddaf109e19f2f47d5e4eaa
David Van de Ven <zanshin.g1@gmail.com>
2012-06-26 17:21:13 -0700

    Missed TabListener.
    Tweaks to main layouts to take advantage of variable service fragment size.

---
+++ wififixer/src/org/wahtod/wififixer/ui/TabListener.java
@@ -0,0 +1,67 @@
+/*Copyright [2010-2012] [David Van de Ven]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+
+package org.wahtod.wififixer.ui;
+
+import org.wahtod.wififixer.R;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+
+public class TabListener implements ActionBar.TabListener {
+	private final Activity mActivity;
+	private final ViewPager mPager;
+
+	public TabListener(Activity activity) {
+		mActivity = activity;
+		mPager = (ViewPager) mActivity.findViewById(R.id.pager);
+		/*
+		 * Throwing in a page change listener so the viewpager can keep the
+		 * currently selected tab synched with its current page 
+		 */
+		if (mPager != null) {
+			mPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+				@Override
+				public void onPageScrollStateChanged(int arg0) {
+				}
+
+				@Override
+				public void onPageScrolled(int arg0, float arg1, int arg2) {
+				}
+
+				@Override
+				public void onPageSelected(int arg0) {
+					mActivity.getActionBar().setSelectedNavigationItem(arg0);
+				}
+			});
+		}
+	}
+
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		if (mPager != null) {
+			mPager.setCurrentItem(tab.getPosition());
+		}
+	}
+
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
+}
