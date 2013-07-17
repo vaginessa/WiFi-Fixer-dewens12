9c8828d02b324aa11fdb18c42f9cfe96c97e32b8
zanshin <zanshin.g1@gmail.com>
2013-07-17 05:59:28 -0700

    Bugfix: Crash when sleep policy set on API 17+

--- wififixer/src/org/wahtod/wififixer/legacy/JellyBeanSleepPolicy.java
+++ wififixer/src/org/wahtod/wififixer/legacy/JellyBeanSleepPolicy.java
@@ -1,8 +1,11 @@
 package org.wahtod.wififixer.legacy;
 
+import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.provider.Settings;
+import org.wahtod.wififixer.utility.NotifUtil;
 
 /**
  * Created by zanshin on 7/11/13.
@@ -11,9 +14,7 @@ import android.provider.Settings;
 public class JellyBeanSleepPolicy extends SleepPolicyHelper {
     @Override
     public void vSetSleepPolicy(Context context, int policy) {
-        ContentResolver cr = context.getContentResolver();/**/
-        android.provider.Settings.Global.putInt(cr, Settings.Global.WIFI_SLEEP_POLICY, policy);
-
+        NotifUtil.show(context,getSleepPolicyString(policy), "Tap to set",8124,getPendingIntent(context));
     }
 
     @Override
@@ -26,5 +27,30 @@ public class JellyBeanSleepPolicy extends SleepPolicyHelper {
             policy = -1;
         }
         return policy;
+    }
+
+    private PendingIntent getPendingIntent(Context context) {
+        Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+        PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
+        return pending;
+    }
+
+    private String getSleepPolicyString(int policy) {
+        String out = "Set Sleep Policy in Advanced to: ";
+
+        switch (policy) {
+            case Settings.Global.WIFI_SLEEP_POLICY_DEFAULT:
+                out += "When screen is off";
+                break;
+
+            case Settings.Global.WIFI_SLEEP_POLICY_NEVER_WHILE_PLUGGED:
+                out += "Never when Plugged";
+                break;
+
+            case Settings.Global.WIFI_SLEEP_POLICY_NEVER:
+                out += "Never";
+                break;
+        }
+        return out;
     }
 }
