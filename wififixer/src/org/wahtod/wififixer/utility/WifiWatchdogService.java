6c9dea74f9fc6381dffd7f8483c33a171c45c52e
zanshin <zanshin.g1@gmail.com>
2013-08-03 18:38:40 -0700

    Forgot to add copyright settings when I reinstalled Android Studio

--- wififixer/src/org/wahtod/wififixer/utility/WifiWatchdogService.java
+++ wififixer/src/org/wahtod/wififixer/utility/WifiWatchdogService.java
@@ -1,3 +1,21 @@
+/*
+ * Wifi Fixer for Android
+ *     Copyright (C) 2010-2013  David Van de Ven
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see http://www.gnu.org/licenses
+ */
+
 package org.wahtod.wififixer.utility;
 
 import android.app.PendingIntent;
@@ -108,11 +126,6 @@ public class WifiWatchdogService extends Service {
         }
 
         return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
     }
 
     @Override
